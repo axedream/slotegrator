a32e69c1e7e6853df17e04732e6e864c0f86fac3
Igor Sharendo <axe_dream@list.ru>
2020-12-05 23:04:18 +0700

    hotfix error from e-mail send on master server

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -55,7 +55,8 @@ class SignupForm extends Model
         $user->setPassword($this->password);
         $user->generateAuthKey();
         $user->generateEmailVerificationToken();
-        return $user->save() && $this->sendEmail($user);
+        //return $user->save() && $this->sendEmail($user);
+        return $user->save();
 
     }
 
