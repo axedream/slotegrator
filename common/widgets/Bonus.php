e9c478ef3f08c0363aef1609ffe91cb1b09b0a46
Igor Sharendo <axe_dream@list.ru>
2020-12-07 15:34:12 +0700

    общий концепт получения бонуса

--- common/widgets/Bonus.php
+++ common/widgets/Bonus.php
@@ -6,8 +6,13 @@ use Yii;
 class Bonus extends \yii\bootstrap\Widget
 {
 
+    public function init_bonus()
+    {
+        return ['action'=>'active'];
+    }
+
     public function run()
     {
-        return $this->render('bonus');
+        return $this->render('bonus',['bonus'=>Yii::$app->user->isGuest ? FALSE : $this->init_bonus()]);
     }
 }
